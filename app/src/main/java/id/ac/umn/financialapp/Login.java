1fa09b35cd20168ade7953aa84d8b2d06b7cf92f
BANGATA <atanasiusditya@gmail.com>
2022-12-03 11:16:47 +0700

    feat: firebase config

--- app/src/main/java/id/ac/umn/financialapp/Login.java
+++ app/src/main/java/id/ac/umn/financialapp/Login.java
@@ -6,17 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void signuphere(View view)
     {
         Intent signuphere = new Intent(Login.this, Signup.class);
         startActivity(signuphere);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+        }
     }
 }
